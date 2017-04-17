307e38938891da22e99de7c3341c490411043737
Craig Hesling <craig@hesling.com>
2017-04-16 21:59:17 -0400

    Added initial parts of AES

--- AES.sv
+++ AES.sv
@@ -5,7 +5,60 @@
 
 typedef enum logic [1:0] {WAIT, PHASE1, PHASE2, DONE} aes_state_t;
 
+typedef logic [4-1:0][4-1:0][8-1:0] block128_t;
+
 /* Should Probably Facilitate The Common AES Phase Modules Here */
+
+module JB_AES_128Block(blockin, blockout);
+    input logic [128-1:0] blockin;
+    output block128_t blockout;
+
+    assign blockout = blockin;
+endmodule
+
+
+module JB_AES_RowShift(blockin, blockout);
+    input  block128_t blockin;
+    output block128_t blockout;
+
+    /* Preform The Rotations */
+    always_comb begin
+        blockout[0] = {blockin[0]}; // rotate none
+        blockout[1] = {blockin[1][2:0], blockin[1][3]}; // rotate once
+        blockout[2] = {blockin[2][1:0], blockin[1][3], blockin[1][2]};
+        blockout[3] = {blockin[2][0], blockin[1][3], blockin[1][2], blockin[1][1]};
+    end
+endmodule
+
+module JB_AES_FirstRound(roundkey, blockin, blockout);
+    input  block128_t roundkey;
+    input  block128_t blockin;
+    output block128_t blockout;
+
+    assign blockout = blockin ^ roundkey;
+endmodule
+
+module JB_AES_Round(roundkey, blockin, blockout);
+    input  block128_t roundkey;
+    input  block128_t blockin;
+    output block128_t blockout;
+
+    // TODO: Implement standard round here
+    assign blockout = blockin ^ roundkey;
+endmodule
+
+module JB_AES_LastRound(roundkey, blockin, blockout);
+    input  block128_t roundkey;
+    input  block128_t blockin;
+    output block128_t blockout;
+
+    // TODO: Implement last round here
+    assign blockout = blockin ^ roundkey;
+endmodule
+
+// module JB_AES_LastRound(key, blockin, blockout) begin
+    
+// end
 
 module JB_AES_Encrypt(clk, nRst, nStart, nDone, key, blockin, blockout);
     parameter BLOCK_WIDTH = 128; // 128, 192, or 256
