3dd6ee58aaed1fcf593bddaa2547d7b9ac3fba10
Craig Hesling <craig@hesling.com>
2017-05-13 21:40:35 -0400

    Added round key

--- AES.sv
+++ AES.sv
@@ -173,7 +173,70 @@ module JB_AES_RoundKey(roundconstants, roundkeyin, roundkeyout);
     input  block128_t roundkeyin;
     output block128_t roundkeyout;
 
-    assign roundkeyout = roundkeyin;
+	logic [7:0] rcon;
+	logic [0:3][7:0] key_col,key_col_rot,key_col_sub;
+
+	//Get last column of key block in
+	always_comb begin
+		for (int i = 0; i < 4; i++) begin
+			key_col[i] = roundkeyin[i][3];
+		end
+	end
+
+	assign key_col_rot = {key_col[1],key_col[2],key_col[3],key_col[0]}; //rotated input column
+
+	subBytes_keygen sbkg0(.key_col_rot(key_col_rot),
+								 .key_col_sub(key_col_sub));
+
+	assign rcon = roundconstants[ROUND-1];
+
+	always_comb begin
+		roundkeyout = '0;
+
+		//Genrerate first column of round key
+		roundkeyout[0][0] = roundkeyin[0][0]^key_col_sub[0]^rcon;
+		for (int i = 1; i < 4; i++) begin
+			roundkeyout[i][0] = roundkeyin[i][0]^key_col_sub[i];
+		end
+
+		for (int i = 1; i < 4; i++) begin //For each column
+			for (int j = 0; j < 4; j++) begin // For each elem in a column
+				roundkeyout[j][i] = roundkeyin[j][i]^roundkeyout[j][i-1]; 
+			end
+		end
+	end
+endmodule
+
+module subBytes_keygen(
+   input logic [0:3][7:0] key_col_rot,
+   output logic [0:3][7:0] key_col_sub
+);
+
+   logic [0:15][0:15][7:0] s_box;
+
+   assign s_box[0] = {8'h63,8'h7c,8'h77,8'h7b,8'hf2,8'h6b,8'h6f,8'hc5,8'h30,8'h01,8'h67,8'h2b,8'hfe,8'hd7,8'hab,8'h76};
+   assign s_box[1] = {8'hca,8'h82,8'hc9,8'h7d,8'hfa,8'h59,8'h47,8'hf0,8'had,8'hd4,8'ha2,8'haf,8'h9c,8'ha4,8'h72,8'hc0};
+   assign s_box[2] = {8'hb7,8'hfd,8'h93,8'h26,8'h36,8'h3f,8'hf7,8'hcc,8'h34,8'ha5,8'he5,8'hf1,8'h71,8'hd8,8'h31,8'h15};
+   assign s_box[3] = {8'h04,8'hc7,8'h23,8'hc3,8'h18,8'h96,8'h05,8'h9a,8'h07,8'h12,8'h80,8'he2,8'heb,8'h27,8'hb2,8'h75};
+   assign s_box[4] = {8'h09,8'h83,8'h2c,8'h1a,8'h1b,8'h6e,8'h5a,8'ha0,8'h52,8'h3b,8'hd6,8'hb3,8'h29,8'he3,8'h2f,8'h84}; 
+   assign s_box[5] = {8'h53,8'hd1,8'h00,8'hed,8'h20,8'hfc,8'hb1,8'h5b,8'h6a,8'hcb,8'hbe,8'h39,8'h4a,8'h4c,8'h58,8'hcf};
+   assign s_box[6] = {8'hd0,8'hef,8'haa,8'hfb,8'h43,8'h4d,8'h33,8'h85,8'h45,8'hf9,8'h02,8'h7f,8'h50,8'h3c,8'h9f,8'ha8};
+   assign s_box[7] = {8'h51,8'ha3,8'h40,8'h8f,8'h92,8'h9d,8'h38,8'hf5,8'hbc,8'hb6,8'hda,8'h21,8'h10,8'hff,8'hf3,8'hd2};
+   assign s_box[8] = {8'hcd,8'h0c,8'h13,8'hec,8'h5f,8'h97,8'h44,8'h17,8'hc4,8'ha7,8'h7e,8'h3d,8'h64,8'h5d,8'h19,8'h73};
+   assign s_box[9] = {8'h60,8'h81,8'h4f,8'hdc,8'h22,8'h2a,8'h90,8'h88,8'h46,8'hee,8'hb8,8'h14,8'hde,8'h5e,8'h0b,8'hdb};
+   assign s_box[10] = {8'he0,8'h32,8'h3a,8'h0a,8'h49,8'h06,8'h24,8'h5c,8'hc2,8'hd3,8'hac,8'h62,8'h91,8'h95,8'he4,8'h79};
+   assign s_box[11] = {8'he7,8'hc8,8'h37,8'h6d,8'h8d,8'hd5,8'h4e,8'ha9,8'h6c,8'h56,8'hf4,8'hea,8'h65,8'h7a,8'hae,8'h08};
+   assign s_box[12] = {8'hba,8'h78,8'h25,8'h2e,8'h1c,8'ha6,8'hb4,8'hc6,8'he8,8'hdd,8'h74,8'h1f,8'h4b,8'hbd,8'h8b,8'h8a};
+   assign s_box[13] = {8'h70,8'h3e,8'hb5,8'h66,8'h48,8'h03,8'hf6,8'h0e,8'h61,8'h35,8'h57,8'hb9,8'h86,8'hc1,8'h1d,8'h9e};
+   assign s_box[14] = {8'he1,8'hf8,8'h98,8'h11,8'h69,8'hd9,8'h8e,8'h94,8'h9b,8'h1e,8'h87,8'he9,8'hce,8'h55,8'h28,8'hdf};
+   assign s_box[15] = {8'h8c,8'ha1,8'h89,8'h0d,8'hbf,8'he6,8'h42,8'h68,8'h41,8'h99,8'h2d,8'h0f,8'hb0,8'h54,8'hbb,8'h16};
+
+   always_comb begin
+      for (int i = 0; i < 4; i++) begin
+         key_col_sub[i]= s_box[key_col_rot[i][7:4]][key_col_rot[i][3:0]];
+      end
+   end
+
 endmodule
 
 /*********************************************************/
